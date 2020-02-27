51f79099528338b3324103b6f9ffd4a76e6917eb
Todd G <todd@dapix.io>
2020-02-27 16:06:46 -0700

    run make in parallel
    the calculation that attempts to calculate cores based on core count vs available memory / 4gb always results in: `make -j1`  Just use the number of cores instead, builds much faster.

--- build.sh
+++ build.sh
@@ -75,10 +75,7 @@ else
    FREE_MEM=`LC_ALL=C free | grep "Mem:" | awk '{print $4}'`
 fi
 
-CORES_AVAIL=`getconf _NPROCESSORS_ONLN`
-MEM_CORES=$(( ${FREE_MEM}/4000000 )) # 4 gigabytes per core
-MEM_CORES=$(( $MEM_CORES > 0 ? $MEM_CORES : 1 ))
-CORES=$(( $CORES_AVAIL < $MEM_CORES ? $CORES_AVAIL : $MEM_CORES ))
+CORES=`getconf _NPROCESSORS_ONLN`
 
 #check submodules
 if [ $(( $(git submodule status --recursive | grep -c "^[+\-]") )) -gt 0 ]; then
