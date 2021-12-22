663d6acfc7b84032478868c57633f921516d7053
adsorptionenthalpy <androulidakisa@gmail.com>
2021-12-21 23:05:15 -0700

    duplicate definition in crypto.hpp

--- libraries/eosiolib/core/eosio/crypto.hpp
+++ libraries/eosiolib/core/eosio/crypto.hpp
@@ -101,35 +101,6 @@ namespace eosio {
 
 
    /**
-    * EOSIO Public Key
-    * @brief EOSIO Public Key
-    */
-   struct public_key {
-      /**
-       * Type of the public key, could be either K1 or R1
-       * @brief Type of the public key
-       */
-      unsigned_int        type;
-
-      /**
-       * Bytes of the public key
-       *
-       * @brief Bytes of the public key
-       */
-      std::array<char,33> data;
-
-      friend bool operator == ( const public_key& a, const public_key& b ) {
-        return std::tie(a.type,a.data) == std::tie(b.type,b.data);
-      }
-      friend bool operator != ( const public_key& a, const public_key& b ) {
-        return std::tie(a.type,a.data) != std::tie(b.type,b.data);
-      }
-      EOSLIB_SERIALIZE( public_key, (type)(data) )
-   };
-
-
-
-   /**
     *  Serialize an eosio::webauthn_public_key into a stream
     *
     *  @ingroup public_key
