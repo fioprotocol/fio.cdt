6c02611e97e438959408995dfa974788e0aa4109
Michael Leet <michael@fioprotocol.io>
2024-05-12 11:48:52 -0600

    BD-4618: minor cleanup

--- utils.sh
+++ utils.sh
@@ -106,9 +106,10 @@ function is-cmake-built() {
     if [[ -x ${FIO_CDT_TMP_DIR}/cmake-${CMAKE_VERSION}/build/bin/cmake ]]; then
         cmake_version=$(${FIO_CDT_TMP_DIR}/cmake-${CMAKE_VERSION}/build/bin/cmake --version | grep version | awk '{print $3}')
         if [[ $cmake_version =~ 3.2 ]]; then
-            #cmake_install_location=$(cat ${FIO_CDT_TMP_DIR}/cmake-${CMAKE_VERSION}/build/CMakeCache.txt | grep CMAKE_INSTALL_PREFIX | awk -F= '{print $2}')
-            #[[ -x $cmake_install_location/bin/cmake ]]
-            #[[ $? -eq 0 ]] && return
+            #cat ${FIO_CDT_TMP_DIR}/llvm4/build/CMakeCache.txt | grep CMAKE_INSTALL_PREFIX | grep ${EOSIO_INSTALL_DIR} >/dev/null
+            #if [[ $? -eq 0 ]]; then
+            #    return
+            #fi
             return
         fi
     fi
