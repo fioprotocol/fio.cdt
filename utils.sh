9fea78ce2114a4784e2231b35b6fa28546fb9752
Michael Leet <michael@fio.net>
2025-04-14 22:46:52 -0600

    Turn off openssl build and install

--- utils.sh
+++ utils.sh
@@ -87,7 +87,7 @@ OPENSSL_TAG_NAME=OpenSSL_1_1_1w
 OPENSSL_ROOT=/usr/local/ssl
 # Check openssl root (install dir) for openssl, otherwise, download, build and install
 function ensure-openssl() {
-    if $BUILD_OPENSSL; then
+    if $DO_OPENSSL; then
         echo "${COLOR_CYAN}[Ensuring OpenSSL support]${COLOR_NC}"
         if ! is-openssl-installed; then
             # Check tmp dir for previous openssl build
